787afd1e7de8d9b6531f1d1e90c6a81f401b6633
UmutSogukpinar <usogukpinar0@gmail.com>
2024-11-19 15:59:21 +0300

    size class added

---
+++ src/Size.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Size
+{
+    int width;
+    int height;
+
+    public Size(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+}
