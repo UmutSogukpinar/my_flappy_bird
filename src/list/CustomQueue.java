d15b31cde4389b8225cd82dbd5d91695738afb84
UmutSogukpinar <usogukpinar0@gmail.com>
2024-11-22 12:56:55 +0300

    custom queue functions added

--- src/list/CustomQueue.java
+++ src/list/CustomQueue.java
@@ -24,5 +24,29 @@ public class CustomQueue<T>
         first = null;
         last = null;
     }
-    
+
+    // add element end of the queue
+    public void enqueue(T data)
+    {
+        Node newNode = new Node(data);
+
+        if (last == null)
+            first = newNode;
+        else
+            last.next = newNode;
+
+        last = newNode;
+    }
+
+    public void updateQueue()
+    {
+        if (first == null || first.next == null)
+            return;
+
+        Node firstNode = first;
+        first = first.next;
+        last.next = firstNode;
+        last = firstNode;
+        firstNode.next = null;
+    }
 }
